ceddc1a1455e1e30b81bbac51abc21aa56585fc8
Rodrigo Perez <raapperez@gmail.com>
2016-12-11 20:43:32 -0200

    doing forcomp

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -102,7 +102,13 @@ object Anagrams {
    *  Note: the resulting value is an occurrence - meaning it is sorted
    *  and has no zero-entries.
    */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences = {
+    val xMap = x.toMap
+    val char = y.
+    x.toMap.updated(y.head._1, )
+    y.head
+    x.foldLeft()
+  }
 
   /** Returns a list of all anagram sentences of the given sentence.
    *
